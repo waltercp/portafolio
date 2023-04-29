31d4dd3caaacb2a46750527b6933d1fbfba42d50
waltercp <walter_cp_96@hotmail.com>
2023-04-29 01:27:00 -0600

    aboutMe

--- src/componets/AboutMe.jsx
+++ src/componets/AboutMe.jsx
@@ -5,7 +5,7 @@ import './styles/aboutMe.css'
 const AboutMe = () => {
 
   const handleCV =() => {
-    window.open('https://drive.google.com/file/d/1mk_XnwaPuyUZGqTtBDpnOhJpb4dAyEPd/view?usp=sharing', 
+    window.open('https://drive.google.com/file/d/1JUJOSFjDDyiONdnfSfeXSN2gyvEYt-J-/view?usp=sharing', 
                 '_blank')
   }
   return (
@@ -16,7 +16,7 @@ const AboutMe = () => {
         </div>
         <div className='aboutMe-Info'>
           <h2>About <span>Me</span> </h2>
-          <p>Hola! Mi nombre es  Walter Antonio Camacho Pacheco, recién graduado en Ingeniería Informática y en búsqueda de oportunidades para realizar mis prácticas pre profesionales. Me considero una persona proactiva, capaz de adaptarme rápidamente a situaciones nuevas y trabajar en equipo de manera efectiva para alcanzar objetivos comunes. Estoy altamente motivado para aprender y mejorar mis habilidades técnicas y blandas, y así contribuir al crecimiento y éxito de la organización. Agradezco de antemano la oportunidad de demostrar mi potencial.</p>
+          <p>Hola! Mi nombre es  Walter Antonio Camacho Pacheco. Me considero una persona proactiva, capaz de adaptarme rápidamente a situaciones nuevas y trabajar en equipo de manera efectiva para alcanzar objetivos comunes. Estoy altamente motivado para aprender y mejorar mis habilidades técnicas y blandas, y así contribuir al crecimiento y éxito de la organización. Agradezco de antemano la oportunidad de demostrar mi potencial.</p>
           <button onClick={handleCV} type="button">See CV</button>
         </div>
         
