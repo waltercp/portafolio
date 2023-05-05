8e73255834e7696d915bb24c6dc871264236b856
waltercp <walter_cp_96@hotmail.com>
2023-05-04 23:34:50 -0600

    pokemonFondo

--- src/componets/ProyectList.jsx
+++ src/componets/ProyectList.jsx
@@ -37,7 +37,7 @@ const proyectCinco = {
 }
 
 const proyectSeis = {
-  img: '/proyects/proyecto6',
+  img: '/proyects/proyecto6.png',
   name: 'Pokemon',
   descripcion: 'JavaScript | React |  HTML5 | CSS | Redux',
   url: 'https://profound-marshmallow-5a07b8.netlify.app/#/pokedex'
