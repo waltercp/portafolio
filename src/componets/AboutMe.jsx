68c4b8f8a4b2b8a4c9876f605e1e673f627e913b
waltercp <walter_cp_96@hotmail.com>
2023-05-17 00:42:12 -0600

    sobre mi

--- src/componets/AboutMe.jsx
+++ src/componets/AboutMe.jsx
@@ -4,9 +4,9 @@ import './styles/aboutMe.css'
 
 const AboutMe = () => {
 
-  const handleCV =() => {
-    window.open('https://drive.google.com/file/d/1JUJOSFjDDyiONdnfSfeXSN2gyvEYt-J-/view?usp=sharing', 
-                '_blank')
+  const handleCV = () => {
+    window.open('https://drive.google.com/file/d/1JUJOSFjDDyiONdnfSfeXSN2gyvEYt-J-/view?usp=sharing',
+      '_blank')
   }
   return (
     <div className='aboutMe' id='aboutMe'>
@@ -16,10 +16,11 @@ const AboutMe = () => {
         </div>
         <div className='aboutMe-Info'>
           <h2>About <span>Me</span> </h2>
-          <p>Hola! Mi nombre es  Walter Antonio Camacho Pacheco, recién graduado en Ingeniería Informática y en búsqueda de oportunidades para realizar mis prácticas pre profesionales. Me considero una persona proactiva, capaz de adaptarme rápidamente a situaciones nuevas y trabajar en equipo de manera efectiva para alcanzar objetivos comunes. Estoy altamente motivado para aprender y mejorar mis habilidades técnicas y blandas, y así contribuir al crecimiento y éxito de la organización. Agradezco de antemano la oportunidad de demostrar mi potencial.</p>
+          <p>¡Hola! Mi nombre es Walter Antonio Camacho Pacheco y recientemente he completado mi carrera en Ingeniería Informática. Actualmente, estoy buscando oportunidades para realizar mis prácticas pre profesionales. Me considero una persona proactiva y me adapto rápidamente a nuevas situaciones. Trabajar en equipo es algo en lo que me desenvuelvo de manera efectiva, siempre enfocado en alcanzar objetivos comunes.
+            Estoy altamente motivado para seguir aprendiendo y mejorar tanto mis habilidades técnicas como blandas. Creo en el constante crecimiento y deseo contribuir al éxito de la organización donde tenga la oportunidad de demostrar mi potencial.</p>
           <button onClick={handleCV} type="button">See CV</button>
         </div>
-        
+
       </div>
 
     </div>
@@ -28,3 +29,4 @@ const AboutMe = () => {
 }
 
 export default AboutMe
+
